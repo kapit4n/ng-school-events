c2a0d015dd2b43f39117b7a50c219ec014a38ba6
lartl <luis.arce22@gmail.com>
2018-04-27 22:09:13 -0400

    Adding services

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,33 +1,38 @@
-import { Injectable } from '@angular/core';
-import { AuthService } from 'ngx-auth';
-import { Observable } from 'rxjs/Observable';
-import { of } from "rxjs/observable/of";
+import { Injectable } from "@angular/core";
+import { AuthService } from "ngx-auth";
 import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 import { HttpErrorResponse } from "@angular/common/http";
+import "rxjs/add/observable/of";
+import "rxjs/add/operator/catch";
+import "rxjs/add/operator/logout";
 
 @Injectable()
 export class AuthenticationService implements AuthService {
+  logout: any;
 
   constructor(private http: Http) {}
 
   public isAuthorized(): Observable<boolean> {
-    const isAuthorized: boolean = !!localStorage.getItem('accessToken');
+    const isAuthorized: boolean = !!localStorage.getItem("accessToken");
 
     return Observable.of(isAuthorized);
   }
 
   public getAccessToken(): Observable<string> {
-    const accessToken: string = localStorage.getItem('accessToken');
+    const accessToken: string = localStorage.getItem("accessToken");
 
     return Observable.of(accessToken);
   }
 
   public refreshToken(): Observable<any> {
-    const refreshToken: string = localStorage.getItem('refreshToken');
+    const refreshToken: string = localStorage.getItem("refreshToken");
+
+    let thisA = this;
 
     return this.http
-      .post('http://localhost:3001/refresh-token', { refreshToken })
-      .catch(() => this.logout())
+      .post("http://localhost:3001/refresh-token", { refreshToken })
+      .catch(() => thisA.logout());
   }
 
   public refreshShouldHappen(response: HttpErrorResponse): boolean {
@@ -35,7 +40,6 @@ export class AuthenticationService implements AuthService {
   }
 
   public verifyTokenRequest(url: string): boolean {
-    return url.endsWith('refresh-token');
+    return url.endsWith("refresh-token");
   }
-
 }
