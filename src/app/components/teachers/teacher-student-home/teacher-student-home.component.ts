4fb36ae21fa7fe6fe76cbefd4d39139459b44489
lartl <luis.arce22@gmail.com>
2018-07-06 23:58:50 -0400

    Updated teacher student home to display follow up table and modal

--- src/app/components/teachers/teacher-student-home/teacher-student-home.component.ts
+++ src/app/components/teachers/teacher-student-home/teacher-student-home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-teacher-student-home',
-  templateUrl: './teacher-student-home.component.html',
-  styleUrls: ['./teacher-student-home.component.css']
+  selector: "app-teacher-student-home",
+  templateUrl: "./teacher-student-home.component.html",
+  styleUrls: ["./teacher-student-home.component.css"]
 })
 export class TeacherStudentHomeComponent implements OnInit {
+  student = { firstName: "First Name", lastName: "Last Name" };
+  followUps = [
+    {
+      id: 1,
+      registereddDate: "1-1-1",
+      title: "Title",
+      subject: "Subject",
+      isDelayed: true,
+      isAbsent: false,
+      observation: "Subject "
+    },
+    {
+      id: 2,
+      registereddDate: "2-2-2",
+      title: "Title 1",
+      subject: "Subject 1",
+      isDelayed: true,
+      isAbsent: false,
+      observation: "Subject 1"
+    }    
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
