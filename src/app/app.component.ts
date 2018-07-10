975d2dd24134ee73e26b08fd462ac8dd05766c9f
lartl <luis.arce22@gmail.com>
2018-07-09 23:14:07 -0400

    Socket io example implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { setTheme } from "ngx-bootstrap/utils";
 import { AuthService } from "./services/auth.service";
 import { RolesService } from "./services/roles.service";
@@ -6,13 +6,25 @@ import { ParentsService } from "./services/parents.service";
 import { Location } from "@angular/common";
 import { Router } from "@angular/router";
 import { ActivatedRoute, NavigationEnd } from "@angular/router";
+import { SocketService } from "./services/socket.service";
+
+enum Action {
+  JOINED,
+  LEFT,
+  RENAME
+}
+
+enum Event {
+  CONNECT = 'connect',
+  DISCONNECT = 'disconnect'
+}
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "app";
   parentToggle = "";
   teacherToggle = "";
@@ -21,12 +33,19 @@ export class AppComponent {
   breadcrumbs: any;
   sons = [];
 
+  action = Action;
+  user: any;
+  messages: any[] = [];
+  messageContent: string;
+  ioConnection: any;
+
   constructor(
     public authSvc: AuthService,
     public rolesSvc: RolesService,
     public parentsSvc: ParentsService,
     private location: Location,
-    private router: ActivatedRoute
+    private router: ActivatedRoute,
+    private socketService: SocketService
   ) {
     setTheme("bs4");
     this.userType = this.rolesSvc.getUserType();
@@ -48,8 +67,51 @@ export class AppComponent {
     }, 500);
   }
 
+  ngOnInit(): void {
+    this.initIoConnection();
+  }
+
   reloadUrl(sonUrl) {
     location.href = "" + sonUrl;
   }
 
+  private initIoConnection(): void {
+    this.socketService.initSocket();
+
+    this.ioConnection = this.socketService
+      .onMessage()
+      .subscribe((message: any) => {
+        console.log(message);
+        this.messages.push(message);
+      });
+    //chat message
+    this.socketService.onEvent(Event.CONNECT).subscribe(() => {
+      console.log("connected");
+    });
+    
+    this.socketService.onEvent("message").subscribe(() => {
+      console.log("chat message");
+    });
+
+    this.socketService.onEvent(Event.DISCONNECT).subscribe(() => {
+      console.log("disconnected");
+    });
+  }
+
+  sendMessageVoid() {
+    console.log("This ii");
+    this.sendMessage("This is a message");
+  }
+
+  public sendMessage(message: string) {
+    if (!message) {
+      return;
+    }
+
+    this.socketService.send({
+      from: this.rolesSvc.getUserName(),
+      content: message
+    });
+    this.messageContent = null;
+  }
 }
---
+++ src/app/services/socket.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { Observer } from "rxjs/Observer";
+
+import * as socketIo from "socket.io-client";
+
+const SERVER_URL = "http://localhost:3000";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private socket;
+
+  public initSocket(): void {
+    this.socket = socketIo(SERVER_URL);
+  }
+
+  public send(message: any): void {
+    this.socket.emit('message', message);
+  }
+
+  public onMessage(): Observable<any> {
+    return new Observable<any>(observer => {
+      this.socket.on('message', (data: any) => observer.next(data));
+    });
+  }
+
+  public onEvent(event: any): Observable<any> {
+    return new Observable<any>(observer => {
+      this.socket.on(event, () => observer.next());
+    });
+  }
+}
