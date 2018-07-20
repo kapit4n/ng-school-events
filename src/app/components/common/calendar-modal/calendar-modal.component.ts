57ea8e43c9a777b4d3bfeb534db7cf6910dd4026
Viko Dev <viko.dev.z@gmail.com>
2018-07-19 22:59:19 -0400

    US-010 Partial Implementation for Annoucement Per Course
    US-010 Partial Implementation for Annoucement Per Course

--- src/app/components/common/calendar-modal/calendar-modal.component.ts
+++ src/app/components/common/calendar-modal/calendar-modal.component.ts
@@ -14,12 +14,35 @@ import {Announcement} from '../announcements.model';
 export class CalendarModalComponent implements OnInit {
   closeResult: string;
   inputsForm: FormGroup;
+  inputsCourseForm: FormGroup;
   title: string;
   description: string;
   startDateField: Date;
   durationField: number;
   announcement = new Announcement();
   @Output() passData: EventEmitter<Object> = new EventEmitter();
+  currentAction: string;
+
+  // multi-select
+  // itemList = [];
+  // selectedItems = [];
+  // settings = {};
+  itemList = [
+    { "id": 1, "itemName": "Angular" },
+    { "id": 2, "itemName": "JavaScript" },
+    { "id": 3, "itemName": "HTML" },
+    { "id": 4, "itemName": "CSS" },
+    { "id": 5, "itemName": "ReactJS" },
+    { "id": 6, "itemName": "HTML5" }
+  ];
+  selectedItems = [];
+  settings = {
+    text: "Select Skills",
+    selectAllText: 'Select All',
+    unSelectAllText: 'UnSelect All',
+    classes: "myclass custom-class"
+  };
+  @Output() passCFData: EventEmitter<Object> = new EventEmitter();
 
   constructor(private modalService: NgbModal, private fb: FormBuilder) {
   }
@@ -35,9 +58,17 @@ export class CalendarModalComponent implements OnInit {
       startDateField: ['', [Validators.required]],
       durationField: ['', [Validators.required, this.checkDuration]]
     });
+    this.inputsCourseForm = this.fb.group({
+      skills: [[], Validators.required],
+      title: ['', [Validators.required]],
+      description: [''],
+      startDateField: ['', [Validators.required]],
+      durationField: ['', [Validators.required, this.checkDuration]]
+    });
   }
 
-  open(content) {
+  open(action: string, content) {
+    this.currentAction = action;
     this.modalService.open(content).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
@@ -66,6 +97,18 @@ export class CalendarModalComponent implements OnInit {
     this.inputsForm.reset();
   }
 
+  setCFValues() {
+    // this.announcement.id = this.generateId();
+    // this.announcement.title = this.inputsForm.get('title').value;
+    // this.announcement.description = this.inputsForm.get('description').value;
+    // this.announcement.startDate = this.inputsForm.get('startDateField').value;
+    // this.announcement.endDate = this.inputsForm.get('durationField').value;
+    // this.announcement.endDate = this.addDays(this.announcement.startDate, this.announcement.endDate);
+    // this.passData.emit(this.announcement);
+    this.passCFData.emit(this.inputsCourseForm.getRawValue());
+    this.inputsCourseForm.reset();
+  }
+
   generateId(): string {
     const id = (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase();
     return id;
@@ -73,6 +116,10 @@ export class CalendarModalComponent implements OnInit {
 
   reset() {
     this.inputsForm.reset();
+  }
+
+  resetCF() {
+    this.inputsCourseForm.reset();
   }
 
   private addDays(date: any, days: number ): Date {
@@ -88,5 +135,22 @@ export class CalendarModalComponent implements OnInit {
     } else {
       return null;
     }
+  }
+
+  // multi-select
+
+  onItemSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  OnItemDeSelect(item: any) {
+    console.log(item);
+    console.log(this.selectedItems);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+  onDeSelectAll(items: any) {
+    console.log(items);
   }
 }
