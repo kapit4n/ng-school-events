9e4468448a8d887e8474c4881beaee3a8fd7c513
lartl <luis.arce22@gmail.com>
2018-07-21 09:21:41 -0400

    Display new question popup

--- src/app/components/parent/question-home/question-home.component.ts
+++ src/app/components/parent/question-home/question-home.component.ts
@@ -4,21 +4,23 @@ import { QuestionsService } from "../../../services/questions.service";
 import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-question-home',
-  templateUrl: './question-home.component.html',
-  styleUrls: ['./question-home.component.css']
+  selector: "app-question-home",
+  templateUrl: "./question-home.component.html",
+  styleUrls: ["./question-home.component.css"]
 })
 export class QuestionHomeComponent implements OnInit {
-
   studentId = "0";
   course: any;
   questions = [];
+  newQuestion: any;
 
-  constructor(private route: ActivatedRoute,
-      private parentsSvc: ParentsService,
-      private questionsSvc: QuestionsService
-  ) { 
+  constructor(
+    private route: ActivatedRoute,
+    private parentsSvc: ParentsService,
+    private questionsSvc: QuestionsService
+  ) {
     this.course = {};
+    this.newQuestion = {};
   }
 
   ngOnInit() {
@@ -26,11 +28,14 @@ export class QuestionHomeComponent implements OnInit {
     this.parentsSvc.getCourseByStudentId(this.studentId).subscribe(courses => {
       this.course = courses[0];
       this.questionsSvc
-        .getQuestions(this.course['course-year'].courseId)
+        .getQuestions(this.course["course-year"].courseId)
         .subscribe(questions => {
           this.questions = questions;
         });
     });
   }
 
+  saveQuestion() {
+    console.log(this.newQuestion);
+  }
 }
