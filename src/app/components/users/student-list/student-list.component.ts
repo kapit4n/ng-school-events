4dd0b296da1ab4e593fe62fa4b89e87c22980ce6
lartl <luis.arce22@gmail.com>
2018-07-23 01:07:30 -0400

    Add student by popup

--- src/app/components/users/student-list/student-list.component.ts
+++ src/app/components/users/student-list/student-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StudentsService } from "../../../services/students.service";
 import { ConfigurationService } from "../../../services/configuration.service";
 import { ActivatedRoute } from "@angular/router";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "app-student-list",
@@ -11,6 +12,8 @@ import { ActivatedRoute } from "@angular/router";
 export class StudentListComponent implements OnInit {
   students: any[];
   searchText = "";
+  newStudent = {};
+  closeResult: string;
 
   pages = 0;
   currentPage = 1;
@@ -19,6 +22,7 @@ export class StudentListComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private studentsSvc: StudentsService,
+    private modalService: NgbModal,
     private confSvc: ConfigurationService
   ) {}
 
@@ -39,19 +43,44 @@ export class StudentListComponent implements OnInit {
       .subscribe(students => {
         this.students = students;
       });
-      
-    this.studentsSvc
-      .getStudentsCount(this.searchText)
-      .subscribe(countInfo => {
-        this.pages = Math.round(countInfo.count / this.confSvc.pageSize);
-        const range = (from, to, step) =>
-          Array(Math.floor((to - from) / step) + 1)
-            .fill(0)
-            .map((v, i) => from + i * step);
-        this.rangePages = range(1, this.pages, 1);
-      });
+
+    this.studentsSvc.getStudentsCount(this.searchText).subscribe(countInfo => {
+      this.pages = Math.round(countInfo.count / this.confSvc.pageSize);
+      const range = (from, to, step) =>
+        Array(Math.floor((to - from) / step) + 1)
+          .fill(0)
+          .map((v, i) => from + i * step);
+      this.rangePages = range(1, this.pages, 1);
+    });
   }
 
+  saveStudent() {
+    this.studentsSvc.registerStudent(this.newStudent).subscribe(student => {
+      this.loadStudents();
+    });
+  }
+
+  open(content) {
+    this.modalService.open(content).result.then(
+      result => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      reason => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+  
   search() {
     this.loadStudents();
   }
