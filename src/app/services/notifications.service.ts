20543ed21054bacffd22a28fdbab48547147e5e8
lartl <luis.arce22@gmail.com>
2018-07-31 21:01:40 -0400

    Updating notifications service

--- src/app/services/notifications.service.ts
+++ src/app/services/notifications.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClientService } from "./http-client.service";
+import { ConfigurationService } from "./configuration.service";
+import { AuthService } from "./auth.service";
+import { Observable } from 'rxjs/Rx';
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/timeout";
+import 'rxjs/add/observable/empty';
 
 @Injectable()
 export class NotificationsService {
 
-  constructor() { }
+  constructor(private configSvc: ConfigurationService, private authSvc: AuthService, private http: HttpClientService) { }
+
+  public registerNotification(notification: any): Observable<any> {
+    return this.http
+      .post(this.configSvc.backendUrl + "/notifications", notification)
+      .map(res => res.json());
+  }
+
+  public removeNotification(notifId): Observable<any> {
+    return this.http
+      .delete(`${this.configSvc.backendUrl}/notifications/${notifId}`)
+      .map(res => res.json());
+  }
 
 }
